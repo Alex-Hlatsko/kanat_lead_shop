fb9f80168eee408025e92b28b3132287c52fa8a0
Alen-Berg <alexei.hlatsko@gmail.com>
2023-08-09 19:05:38 +0200

    buttons and count circle + button with price

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,18 +3,25 @@ import { NavLink } from 'react-router-dom'
 import { useSelector, useDispatch } from 'react-redux';
 import { increment, decrement } from '../store/products/productsSlice';
 
-const Product = ({id, title, price, img}) => {
+const Product = ({ id, title, price, img }) => {
 
   const dispatch = useDispatch();
   const productCount = useSelector((state) => state.products.products[id])
-  
+
   return (
     <div className='product'>
-        {productCount !== 0 ? <p>{productCount}</p> : ''}
-        <img src={img} />
-        <p>{title} ({price}$)</p>
-        <button onClick={() => {dispatch(increment(id))}}>ADD</button>
-        {productCount !== 0 ? <button onClick={() => {dispatch(decrement(id))}}>DEL</button> : ''}
+      {productCount !== 0 ?
+        <div className="product_count">
+          <p>{productCount}</p>
+        </div>
+        :
+        ''
+      }
+
+      <img src={img} />
+      <p>{title} ({price}$)</p>
+      {productCount !== 0 ? <button className='product_btn btn_del' onClick={() => { dispatch(decrement(id)) }}>DEL</button> : ''}
+      <button className='product_btn btn_add' onClick={() => { dispatch(increment(id)) }}>ADD</button>
     </div>
   )
 }
--- src/screens/ProductsScreen.jsx
+++ src/screens/ProductsScreen.jsx
@@ -1,18 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import axios from "axios";
 import Product from '../components/Product'
+import { useSelector } from 'react-redux';
 
 const ProductsScreen = () => {
 
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
 
+  const productCount = useSelector((state) => state.products.products)
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get("https://kanatlead-default-rtdb.firebaseio.com/products.json");
         const productsArray = Object.values(response.data);
-        setProducts(productsArray );
+        setProducts(productsArray);
         setLoading(false);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -23,23 +26,47 @@ const ProductsScreen = () => {
     fetchData();
   }, []);
 
-  return (
-    <div className='container'>
+  const calculateTotalCost = () => {
+    let totalCost = 0;
 
-    {loading ? (
-        <p>Loading...</p>
-        ) 
-        : 
-        products.map((product) => (
-          <Product
-            key={product.id}
-            id={product.id}
-            title={product.title}
-            price={product.price}
-            img={product.imgUrl}
-          />
-        ))}
-    </div>
+    for (const itemId in productCount) {
+      const quantity = productCount[itemId];
+      const product = products.find((item) => item.id === itemId);
+
+      if (product) {
+        totalCost += quantity * product.price;
+      }
+    }
+
+    return totalCost;
+  };
+
+  return (
+    <>
+      <div className='container'>
+        {loading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            {products.map((product) => (
+              <Product
+                key={product.id}
+                id={product.id}
+                title={product.title}
+                price={product.price}
+                img={product.imgUrl}
+              />
+            ))}
+          </>
+        )}
+      </div>
+      {calculateTotalCost() > 0 && (
+        <div className="sell_btn">
+          <p>Total Cost: {calculateTotalCost()}$</p>
+        </div>
+      )}
+    </>
+
   )
 }
 
