5f0d8df9fbd82dceb15bf20494e6c443027ba662
Alen-Berg <alexei.hlatsko@gmail.com>
2023-08-20 19:56:10 +0200

    fixed paths to img

--- src/screens/GeoScreen.jsx
+++ src/screens/GeoScreen.jsx
@@ -7,12 +7,12 @@ const GeoScreen = () => {
 
   return (
     <div className='container'>
-        <Flag img="../public/flags/pl.svg" flag_key="PL" productId={productId} title="Польша"/>
-        <Flag img="../public/flags/de.svg" flag_key="DE" productId={productId} title="Германия"/>
-        <Flag img="../public/flags/jp.svg" flag_key="JP" productId={productId} title="Япония"/>
-        <Flag img="../public/flags/be.svg" flag_key="BE" productId={productId} title="Бельгия"/>
-        <Flag img="../public/flags/nl.svg" flag_key="NL" productId={productId} title="Нидерланды"/>
-        <Flag img="../public/flags/se.svg" flag_key="SE" productId={productId} title="Швеция"/>
+        <Flag img="./flags/pl.svg" flag_key="PL" productId={productId} title="Польша"/>
+        <Flag img="./flags/de.svg" flag_key="DE" productId={productId} title="Германия"/>
+        <Flag img="./flags/jp.svg" flag_key="JP" productId={productId} title="Япония"/>
+        <Flag img="./flags/be.svg" flag_key="BE" productId={productId} title="Бельгия"/>
+        <Flag img="./flags/nl.svg" flag_key="NL" productId={productId} title="Нидерланды"/>
+        <Flag img="./flags/se.svg" flag_key="SE" productId={productId} title="Швеция"/>
         <NavLink to="../">
           <div className="back_btn">
             Back
